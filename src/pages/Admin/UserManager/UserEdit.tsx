ac95b9aa54c1665b7f80cd8adeaa4c885f8c5adc
JorgeAlleres <alleresjorge10@gmail.com>
2025-02-19 14:38:19 +0100

    Cambio de UserManager por Edit/list para mayor simplicidad

---
+++ src/pages/Admin/UserManager/UserEdit.tsx
@@ -0,0 +1,140 @@
+import { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { UserService } from '../../../services/user.service';
+
+function UserEdit() {
+    const { id } = useParams(); // Para obtener el ID de la sala desde la URL
+    const [userName, setUserName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [role, setRole] = useState<string>('');
+    const [active, setActive] = useState<boolean>(false);
+    const [acceptNotifications, setAcceptNotifications] = useState<boolean>(false);
+
+    const navigate = useNavigate();
+
+    // Cargar los datos de la sala al montar el componente
+    useEffect(() => {
+        async function call() {
+            try {
+                const user = await UserService.getById(Number(id));
+                setUserName(user.userName ?? '');
+                setEmail(user.email ?? '');
+                setRole(user.role ?? '');
+                setActive(user.active ?? false)
+                setAcceptNotifications(user.acceptNotifications ?? false)
+            } catch (error) {
+                console.error('Error al cargar los datos del user', error);
+            }
+        }
+
+        call();
+    }, [id]);
+
+    const handleSubmit = async () => {
+        if (!userName || !email) {
+            alert("Por favor, complete todos los campos obligatorios.");
+            return;
+        }
+
+        const userData = {
+            userName,
+            email,
+            role,
+            active,
+            acceptNotifications
+        };
+
+        try {
+            await UserService.update(Number(id), userData);
+            navigate('/users');
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white pt-20">
+            <div className="flex flex-col items-center">
+                <div className="p-8 rounded shadow-md w-96">
+                    <h2 className="text-2xl font-semibold mb-4 text-center">Editar User</h2>
+
+                    <div className="mb-4">
+                        <label htmlFor="userName" className="block text-sm font-bold mb-2">
+                            Nombre de Usuario <span className="text-red-500">*</span>
+                        </label>
+                        <input
+                            type="text"
+                            id="userName"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            value={userName}
+                            onChange={(e) => setUserName(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="email" className="block text-sm font-bold mb-2">
+                            Email <span className="text-red-500">*</span>
+                        </label>
+                        <input
+                            type="text"
+                            id="email"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="role" className="block text-sm font-bold mb-2">
+                            Rol
+                        </label>
+                        <input
+                            type="text"
+                            id="role"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            value={role}
+                            onChange={(e) => setRole(e.target.value)}
+                        />
+                    </div>
+
+                    <div className="mb-4 flex items-center gap-2">
+                        <label htmlFor="active" className="text-sm font-bold">
+                            Activo
+                        </label>
+                        <input
+                            type="checkbox"
+                            id="active"
+                            checked={active ? true : false}
+                            onChange={(e) => setActive(e.target.checked)}
+                        />
+                    </div>
+
+                    <div className="mb-4 flex items-center gap-2">
+                        <label htmlFor="acceptNotifications" className="text-sm font-bold">
+                            Acepta Notificaciones
+                        </label>
+                        <input
+                            type="checkbox"
+                            id="acceptNotifications"
+                            checked={acceptNotifications ? true : false}
+                            onChange={(e) => setAcceptNotifications(e.target.checked)}
+                        />
+                    </div>
+
+                    <p className="text-sm italic text-gray-400 mb-4">* Son campos obligatorios</p>
+
+                    <div className="flex items-center justify-center">
+                        <button
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            onClick={handleSubmit}
+                        >
+                            Actualizar user
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default UserEdit;
---
+++ src/pages/Admin/UserManager/UserList.tsx
@@ -0,0 +1,77 @@
+import toast from "react-hot-toast";
+import User from "../../../models/User";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { UserService } from "../../../services/user.service";
+
+function UserList() {
+    const [users, setUsers] = useState<User[]>();
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setLoading(true);
+        UserService.getAll()
+            .then(setUsers)
+            .catch((error) => setError(error.message))
+            .finally(() => setLoading(false));
+    }, []);
+
+    const handleDelete = async (id: number) => {
+        if (!window.confirm("¿Estás seguro que quieres borrar este user?")) return;
+        try {
+            await UserService.delete(id);
+            setUsers((prevUsers) => prevUsers?.filter((user) => user.id !== id));
+            toast.success("User succesfully deleted");
+        } catch (error) {
+            setError(error instanceof Error ? error.message : "Unknown error");
+        }
+    };
+
+    return (
+        <div className="relative overflow-x-auto">
+            {loading && <div className="text-black font-bold mb-4">{loading}</div>}
+            {error && <div className="text-red-500 font-bold mb-4">{error}</div>}
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th className="px-4 py-2">ID</th>
+                        <th className="px-4 py-2">Nombre</th>
+                        <th className="px-4 py-2">Email</th>
+                        <th className="px-4 py-2">Role</th>
+                        <th className="px-4 py-2">Active</th>
+                        <th className="px-4 py-2">Accept Notifications</th>
+                        <th className="px-4 py-2">Gestionar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users?.map(user => (
+                        <tr key={user.id} className="border-b">
+                            <td className="px-4 py-2">{user.id}</td>
+                            <td className="px-4 py-2">{user.userName}</td>
+                            <td className="px-4 py-2">{user.email}</td>
+                            <td className="px-4 py-2">{user.role}</td>
+                            <td className="px-4 py-2">{user.active?<p>✅</p>:<p>❌</p>}</td>
+                            <td className="px-4 py-2">{user.acceptNotifications?<p>✅</p>:<p>❌</p>}</td>
+                            <td className="px-4 py-2">
+                                <div className="p-1">
+                                    <Link
+                                        className="text-yellow-500 font-bold m-2"
+                                        to={`./edit/${user.id}`}>
+                                            Editar
+                                    </Link>
+                                    <button onClick={() => handleDelete(user.id)} className="text-red-500 font-bold">
+                                        Borrar
+                                    </button>
+                                </div>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+
+}
+
+export default UserList;
