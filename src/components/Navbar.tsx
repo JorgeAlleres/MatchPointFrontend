bda3d94531114509cab9e54c03809b269b1bd49d
JorgeAlleres <alleresjorge10@gmail.com>
2025-02-20 14:36:02 +0100

    Adaptacion de navbar a admin/user

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,11 @@ function Navbar() {
   const [msg, setMessage] = useState('');
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
-  //const [userId, setUserId] = useState<string | null>(null);
+
+  const isAdmin = true;
+  {/* TODO: Implementar si el usuario es Admin o no */ }
+  const isAuthenticate = true
+  {/* TODO: Implementar si el usuario esta Autenticado o no */ }
 
   useEffect(() => {
     GameService.getAll()
@@ -34,28 +38,62 @@ function Navbar() {
 
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-50 top-0 left-0 start-0 border-b border-gray-200 dark:border-gray-600">
-      <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
-        {msg}
-        
-        <div className="flex items-center w-full">
-          {/* Select dinámico para juegos */}
-          <select
-            onChange={handleGameChange}
-            className="py-2 px-3 text-gray-900 bg-gray-100 border border-gray-300 rounded-lg hover:bg-gray-200 dark:text-white dark:bg-gray-700 dark:border-gray-600 dark:hover:bg-gray-600 transition-colors"
-            defaultValue=""
-          >
-            <option value="" disabled>Selecciona un juego</option>
-            {games.map((game) => (
-              <option key={game.id} value={game.id}>
-                {game.gameName}
-              </option>
-            ))}
-          </select>
+      {isAuthenticate &&
+        <div className="max-w-screen-xl flex items-center justify-center mx-auto p-4">
+          {msg}
 
-          {/* Insertar un enlace al home o al home admin */}
+          <div className="flex items-center justify-center space-x-4 w-full">
+            {/* Select dinámico para juegos */}
+            {isAdmin ?
+              <Link
+                to={'/roomAdmin'}
+                className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded transition duration-300'
+                >
+                Room Manager
+              </Link>
+              :
+              <select
+                onChange={handleGameChange}
+                className="py-2 px-3 text-gray-900 bg-gray-100 border border-gray-300 rounded-lg hover:bg-gray-200 dark:text-white dark:bg-gray-700 dark:border-gray-600 dark:hover:bg-gray-600 transition-colors"
+                defaultValue=""
+              >
+                <option value="" disabled>Selecciona un juego</option>
+                {games.map((game) => (
+                  <option key={game.id} value={game.id}>
+                    {game.gameName}
+                  </option>
+                ))}
+              </select>
+            }
 
-          {/* Link del perfil */}
-          <div className="ml-auto">
+            {/* Insertar un enlace al GameManager solo para ADMIN */}
+            {isAdmin &&
+              <Link
+                to={'/gameAdmin'}
+                className='bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded transition duration-300'
+                >
+                Game Manager
+              </Link>
+            }
+
+            {/* Insertar un enlace al UserManager solo para ADMIN */}
+            {isAdmin &&
+              <Link
+                to={'/userAdmin'}
+                className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition duration-300'              >
+                User Manager
+              </Link>
+            }
+
+            {/* Insertar un enlace al home o al home admin */}
+            <Link
+              to={isAdmin ? '/admin' : '/'}
+              className='py-2 px-4 text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition-colors'
+            >
+              {isAdmin ? 'HOME ADMIN' : 'HOME'}
+            </Link>
+
+            {/* Link del perfil */}
             <Link
               to="/profile" //TODO No se como abrir el id de mi usuario
               className="py-2 px-4 text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition-colors"
@@ -64,7 +102,7 @@ function Navbar() {
             </Link>
           </div>
         </div>
-      </div>
+      }
     </nav>
   );
 }
