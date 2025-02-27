311a93c9cab1e40422421460f5da4ed9f322ea8f
JorgeAlleres <alleresjorge10@gmail.com>
2025-02-27 13:28:40 +0100

    Cambio variable isAdmin

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ function Navbar() {
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
-  const isAdmin = true;
+  const isAdmin = false;
   {/* TODO: Implementar si el usuario es Admin o no */ }
   const isAuthenticate = true
   {/* TODO: Implementar si el usuario esta Autenticado o no */ }
